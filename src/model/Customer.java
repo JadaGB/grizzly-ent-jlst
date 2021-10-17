3f356c787616c8e5493da6630a986f81470d6c99
JadaGB <jadabaileye@gmail.com>
2021-10-17 17:01:07 -0500

    Added password and event name attributes to Customer class.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -7,6 +7,8 @@ public class Customer {
 	private String LName;
 	private String email;
 	private String phoneNum;
+	private String password;
+	private Event evName;
 	
 	public Customer() {
 		this.cusID = 0000;
@@ -14,14 +16,18 @@ public class Customer {
 		this.LName = "Doe";
 		this.email = "example@gmail.com";
 		this.phoneNum = "000-0000";
+		this.password = "";
+		this.evName = new Event();
 	}
 
-	public Customer(int cusID, String Fname, String LName, String email, String phoneNum) {
+	public Customer(int cusID, String Fname, String LName, String email, String phoneNum, String password, Event evName) {
 		this.cusID = cusID;
 		this.Fname = Fname;
 		this.LName = LName;
 		this.email = email;
 		this.phoneNum = phoneNum;
+		this.password = password;
+		this.evName = evName;
 	}
 	
 	public Customer(Customer obj) {
@@ -30,6 +36,9 @@ public class Customer {
 		this.LName = obj.LName;
 		this.email = obj.email;
 		this.phoneNum = obj.phoneNum;
+		this.password = obj.password;
+		this.evName = obj.evName;
+		
 	}
 
 	public int getCusID() {
@@ -72,11 +81,29 @@ public class Customer {
 		this.phoneNum = phoneNum;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Event getEvName() {
+		return evName;
+	}
+
+	public void setEvName(Event evName) {
+		this.evName = evName;
+	}
+
 	@Override
 	public String toString() {
-		return "Customer ID: " + cusID + ", First Name: " + Fname + ", Last Name: " + LName + ", Email: " + email + ", Phone Number: "
-				+ phoneNum + "\n";
+		return "Customer ID=: " + cusID + ", First Name: " + Fname + ", Last Name: " + LName + ", Email: " + email + ", Phone Number:"
+				+ phoneNum + ", Password: " + password + ", Event Name: " + evName + "\n";
 	}
+
+	
 	
 	
 
