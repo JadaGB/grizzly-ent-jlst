e2726177469dc4a37505d69b959f0ea84b9a1ad9
laciabailey <lacia.bailey@gmail.com>
2021-11-15 21:30:01 -0500

    Added populate request,past transaction, and scheduled table methods
    to EmpdDash class

--- src/view/EmpDash.java
+++ src/view/EmpDash.java
@@ -14,11 +14,18 @@ import javax.swing.JTabbedPane;
 import javax.swing.table.DefaultTableModel;
 
 import controller.SystemController;
+import javax.swing.JTable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.JScrollPane;
 
 public class EmpDash extends JPanel {
 
 	private NewRequestForm newReq;
 	private SystemController controller;
+	private static JTable generalRequesttable;
+	private JTable table_1;
+	private JTable table_2;
 
 	/**
 	 * Create the panel.
@@ -43,19 +50,65 @@ public class EmpDash extends JPanel {
 		Panel panel = new Panel();
 		tabbedPane.addTab("Requests", null, panel, null);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		generalRequesttable = new JTable();
+		scrollPane.setViewportView(generalRequesttable);
+		
 		Panel panel_1 = new Panel();
 		tabbedPane.addTab("Scheduled", null, panel_1, null);
 		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		panel_1.add(scrollPane_1);
+		
+		table_1 = new JTable();
+		scrollPane_1.setViewportView(table_1);
+		
 		Panel panel_2 = new Panel();
 		tabbedPane.addTab("Past Transactions", null, panel_2, null);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		panel_2.add(scrollPane_2);
+		
+		table_2 = new JTable();
+		scrollPane_2.setViewportView(table_2);
 		
 		JLabel lblNewLabel = new JLabel("DASHBOARD");
 		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 22));
 		lblNewLabel.setBounds(257, 10, 153, 42);
 		add(lblNewLabel);
 		
+		tabbedPane.addChangeListener(new ChangeListener() {
+	        public void stateChanged(ChangeEvent e) {
+	            if(tabbedPane.getSelectedIndex()==0) {
+	            	controller.getAllCustRequestsInfo(6);
+	            }
+	            if(tabbedPane.getSelectedIndex()==1) {
+	            	controller.getAllScheduled(6);
+	            }
+	            if(tabbedPane.getSelectedIndex()==2) {
+	            	controller.getAllCustPastTransactions(6);
+	            }
+	        }
+	        
+	    });
+		
 		setVisible(true);
 	}
 
+	public static void populateCustomerRequestTable(String data[][]) {
 
+	//JTable variable name.additemstorow[i](customerName, equipType, equipName, reqDate, quotation, confirmed);
+	String columns[] = { "Name", "Equipment Type","Equipment Name","Qoutation","Requested Date","Confirmed" };
+    DefaultTableModel model = new DefaultTableModel(data,columns);
+
+	 	generalRequesttable.setModel(model);	 	 
+	 	generalRequesttable.setShowGrid(true);
+	 	generalRequesttable.setShowVerticalLines(true);
+	 	
+
+	
+	}
+	
 }
